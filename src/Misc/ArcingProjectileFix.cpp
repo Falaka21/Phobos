11f459a78b2e957677472d3c60a64723e4d1d01e
Meta (X230T) <crabiter@vivaldi.net>
2021-02-20 23:41:46 +0200

    Make distance calculations account Z delta too

---
+++ src/Misc/ArcingProjectileFix.cpp
@@ -0,0 +1,18 @@
+#include <Helpers/Macro.h>
+#include <GeneralStructures.h>
+#include <YRMath.h>
+
+DEFINE_HOOK(6FE4F6, TechnoClass_FireAt_ArcingFix, 0)
+{
+	LEA_STACK(CoordStruct*, coordA, 0x44);
+	LEA_STACK(CoordStruct*, coordB, 0x88);
+
+	double distanceSquared =
+		(double)(coordA->X - coordB->X) * (double)(coordA->X - coordB->X) +
+		(double)(coordA->Y - coordB->Y) * (double)(coordA->Y - coordB->Y) +
+		(double)(coordA->Z - coordB->Z) * (double)(coordA->Z - coordB->Z);
+
+	R->EAX(Math::sqrt(distanceSquared));
+	
+	return 0x6FE537;
+}
