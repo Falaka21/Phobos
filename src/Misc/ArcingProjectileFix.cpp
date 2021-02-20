107d6d9fa83154f5bb6ba016e36a5689a688e76a
Meta (X230T) <crabiter@vivaldi.net>
2021-02-21 00:06:57 +0200

    Cast the double to int

--- src/Misc/ArcingProjectileFix.cpp
+++ src/Misc/ArcingProjectileFix.cpp
@@ -11,8 +11,8 @@ DEFINE_HOOK(6FE4F6, TechnoClass_FireAt_ArcingFix, 0)
 		(double)(coordA->X - coordB->X) * (double)(coordA->X - coordB->X) +
 		(double)(coordA->Y - coordB->Y) * (double)(coordA->Y - coordB->Y) +
 		(double)(coordA->Z - coordB->Z) * (double)(coordA->Z - coordB->Z);
-
-	R->EAX(Math::sqrt(distanceSquared));
+	
+	R->EAX((int)Math::sqrt(distanceSquared));
 	
 	return 0x6FE537;
 }
